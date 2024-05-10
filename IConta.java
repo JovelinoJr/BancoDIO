21ee2f183dc04dba98bc6808cb9bc55c39869fe0
JovelinoJr <143032830+JovelinoJr@users.noreply.github.com>
2024-05-10 10:27:42 -0300

    Add files via upload

---
+++ IConta.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+/**
+ *
+ * @author jgsj
+ */
+public interface IConta {
+	
+	void sacar(double valor);
+	
+	void depositar(double valor);
+	
+	void transferir(double valor, IConta contaDestino);
+	
+	void imprimirExtrato();
+}
